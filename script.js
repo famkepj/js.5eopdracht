8e3cf0044cb1171c5133ba8c260ef0d1d87a9c9a
famkepj <82056762+famkepj@users.noreply.github.com>
2021-06-30 11:43:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const makePizza = function() {
+    console.log ("Make pizza");
+    console.log ("Make pizza bottom");
+    console.log ("Add pizzasause on the bottom");
+    console.log ("Add your favorits vegetables, cheese, meat, fish");
+    console.log ("Put your pizza in the oven")
+};
+
+
+const makeShushi = function() {
+    console.log ("Cook rice in water until it boils. Cover and simmer until grains are soft. Remove from heat and cool.");
+    console.log ("Meanwhile, chop filling into strips or sticks");
+    console.log ("Mix sugar and vinegar. Combine it with cooled rice – this gives it flavour and stickiness.");
+    console.log ("On your sushi bamboo mat, spread out 1 sheet of nori.");
+    console.log ("Cover it as thinly as you can with the rice mixture. Leave an uncovered 2cm strip of nori along the top edge (to seal the roll).");
+    console.log ("In the centre of the nori, lay out horizontal lines of your filling (eg. a line of salmon and a line of cucumber). Don’t overload your sushi with filling.");
+    console.log ("Now for the tricky part. Grip the mat firmly in both hands and roll up sushi as tight as you can. The mat will help you apply even pressure along the roll. Note: don’t roll up the mat into the sushi!");
+    console.log ("Wet the rice-free strip of nori and ‘glue’ it to the sushi roll.");
+    console.log ("Chop the roll into 3cm slices – you’re done!  Repeat steps 4-9 with different fillings for the remaining 3 sheets of nori. You will get better with every roll!");
+};
+
+makeShushi();
+makeShushi();
+makeShushi();
+makePizza();
+makePizza();
